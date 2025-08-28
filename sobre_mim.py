e11e7780836cf93b500f0625d3ac2310c2da6eea
Jeani Abreu <jeaniclery@gmail.com>
2025-08-27 21:22:34 -0300

    Create sobre_mim.py
    Adicionando meu primeiro código Python com o apoio da Suzano

---
+++ sobre_mim.py
@@ -0,0 +1,14 @@
+# Meu primeiro código Python no GitHub 💻
+# Feito por Jeani Abreu ✨
+
+nome = "Jeani"
+curso = "Administração"
+bootcamp = "Python Developer - DIO com apoio da Suzano"
+cidade = "São Luis, MA"
+
+print("👋 Olá, meu nome é", nome)
+print("📚 Faço curso de", curso)
+print("🚀 Estou aprendendo Python no bootcamp:", bootcamp)
+print("📍 Moro em", cidade)
+print("✨ Esse é meu primeiro código no GitHub!")
+
